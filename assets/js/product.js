172a17ecb7bf42a9e987ca51d1c2255fe9b3be6a
Finisher012 <murithik717@gmail.com>
2026-07-24 21:20:03 +0300

    updates

---
+++ assets/js/product.js
@@ -0,0 +1,33 @@
+const products = [
+
+{
+    id:1,
+    name:"Luxury Dress",
+    category:"Dress",
+    price:3800,
+    image:"assets/images/products/product1.jpg",
+    badge:"New",
+    rating:5
+},
+
+{
+    id:2,
+    name:"Elegant Blazer",
+    category:"Women",
+    price:4500,
+    image:"assets/images/products/product2.jpg",
+    badge:"Sale",
+    rating:4.8
+},
+
+{
+    id:3,
+    name:"Leather Bag",
+    category:"Accessories",
+    price:5200,
+    image:"assets/images/products/product3.jpg",
+    badge:"Hot",
+    rating:4.9
+}
+
+];
