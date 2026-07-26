87383e98b5bae1d201324d186a3277f43cab4e4b
Finisher012 <murithik717@gmail.com>
2026-07-26 23:06:10 +0300

    updates

--- assets/js/product.js
+++ assets/js/product.js
@@ -5,7 +5,7 @@ const products = [
     name:"Luxury Dress",
     category:"Dress",
     price:3800,
-    image:"assets/images/products/product1.jpg",
+    image:"https://images.unsplash.com/photo-1779763320651-9b0cd0985543?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fEx1eHVyeSUyMEJsYWNrJTIwRHJlc3N8ZW58MHx8MHx8fDA%3D",
     badge:"New",
     rating:5
 },
@@ -15,7 +15,7 @@ const products = [
     name:"Elegant Blazer",
     category:"Women",
     price:4500,
-    image:"assets/images/products/product2.jpg",
+    image:"https://images.unsplash.com/photo-1578996953841-b187dbe4bc8a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8RWxlZ2FudCUyMEJsYXplcnxlbnwwfHwwfHx8MA%3D%3D",
     badge:"Sale",
     rating:4.8
 },
@@ -25,7 +25,7 @@ const products = [
     name:"Leather Bag",
     category:"Accessories",
     price:5200,
-    image:"assets/images/products/product3.jpg",
+    image:"https://images.unsplash.com/photo-1605733513597-a8f8341084e6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8UHJlbWl1bSUyMExlYXRoZXIlMjBCYWd8ZW58MHx8MHx8fDA%3D",
     badge:"Hot",
     rating:4.9
 }
--- assets/js/shop.js
+++ assets/js/shop.js
@@ -4,42 +4,42 @@ const products = [
 name:"Luxury Black Dress",
 price:7500,
 category:"Dress",
-image:"images/dress1.jpg"
+image:"https://images.unsplash.com/photo-1779763320651-9b0cd0985543?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fEx1eHVyeSUyMEJsYWNrJTIwRHJlc3N8ZW58MHx8MHx8fDA%3D"
 },
 
 {
 name:"Golden Evening Dress",
 price:9800,
 category:"Dress",
-image:"images/dress2.jpg"
+image:"https://media.istockphoto.com/id/659270754/photo/beautiful-woman-in-a-ball-gown.jpg?s=2048x2048&w=is&k=20&c=IWSOxpfquaoehSjMJ4zJo1qGh7czJ2ung7GRwrR7syU="
 },
 
 {
 name:"Classic Ladies Suit",
 price:12000,
 category:"Suit",
-image:"images/suit1.jpg"
+image:"https://plus.unsplash.com/premium_photo-1681484101396-efc86fa953b8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Q2xhc3NpYyUyMExhZGllcyUyMFN1aXR8ZW58MHx8MHx8fDA%3D"
 },
 
 {
 name:"Premium Leather Bag",
 price:6500,
 category:"Bag",
-image:"images/bag1.jpg"
+image:"https://images.unsplash.com/photo-1605733513597-a8f8341084e6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8UHJlbWl1bSUyMExlYXRoZXIlMjBCYWd8ZW58MHx8MHx8fDA%3D"
 },
 
 {
 name:"Luxury Heels",
 price:5800,
 category:"Shoes",
-image:"images/shoe1.jpg"
+image:"https://images.unsplash.com/photo-1524553879936-2ff074ae5816?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8THV4dXJ5JTIwSGVlbHN8ZW58MHx8MHx8fDA%3D"
 },
 
 {
 name:"Designer Handbag",
 price:8200,
 category:"Bag",
-image:"images/bag2.jpg"
+image:"https://images.unsplash.com/photo-1682745230951-8a5aa9a474a0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8RGVzaWduZXIlMjBIYW5kYmFnfGVufDB8fDB8fHww"
 }
 
 ];
